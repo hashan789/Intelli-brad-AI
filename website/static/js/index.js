f382b2938c1fc1e323f4ab0faf73e0d06ef35aca
hashan789 <hashanmalind123@gmail.com>
2023-06-14 11:38:15 +0530

    add the code to send and receive the messages

--- website/static/js/index.js
+++ website/static/js/index.js
@@ -15,6 +15,81 @@ class Chatbox{
         const {openBtn, chatBox, sendBtn} = this.args;
 
         openBtn.addEventListener('click',() => this.toggleState(chatBox))
-        sendBtn.addEventListener()
+        sendBtn.addEventListener('click',() => this.onSendButton(chatBox))
+
+        const node = chatBox.querySelector('input');
+        node.addEventListener('keyup',({key}) => {
+            if(key === 'Enter'){
+                this.onSendButton(chatBox)
+            }
+        })
+    }
+
+    toggleState(chatbox){
+        this.state = !this.state;
+
+        if(this.state){
+            chatbox.classList.add('chatbox-active')
+        }
+        else{
+            chatbox.classList.remove('chatbox-active')
+        }
+    }
+
+    onSendButton(chatbox){
+        var textField = chatbox.querySelector('input');
+        let text1 = textField.value;
+        if (text1 == ""){
+            return;
+        }
+
+        let msg1 = {
+            name : "User",
+            message : text1
+        }
+        this.messages.push(msg1);
+
+        fetch($SCRIPT_ROOT + '/chat', {
+            method : 'POST',
+            body : JSON.stringify({
+                message : text1
+            }),
+            mode : 'cors',
+            headers : {
+                'Content-Type' : 'application/json'
+            },
+        })
+        .then(r => r.json())
+        .then(r => {
+            let msg2 = {
+                name: 'Sam',
+                message: r.answer
+            };
+            this.messages.push(msg2)
+            this.updateChatText(chatbox)
+            textField.value = ''
+        }).catch((error) => {
+            console.error('Error:', error);
+            this.updateChatText(chatbox)
+            textField.value = ''
+        });
+    }
+
+    updateChatText(chatbox){
+        var html = '';
+        this.messages.slice().reverse().forEach(function(item,index){
+            if(item.name === 'Sam'){
+                html += '<div class="msg-item msg-item-visitor">' + item.message + '</div>'
+            }
+            else{
+                html += '<div class="msg-item msg-item-operator">' + item.message + '</div>'
+            }
+        });
+
+        const chatmessage = chatbox.querySelector('.chatbox-msg');
+        chatmessage.innerHTML = html;
     }
 }
+
+const chatbox = new Chatbox();
+chatbox.display();
